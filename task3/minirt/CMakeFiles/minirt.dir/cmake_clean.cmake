3b6326d664afa49de55162bec4be5eebba083422
Muhamedali <79417700+kazakhboy@users.noreply.github.com>
2023-04-21 16:54:10 +0500

    Add files via upload

---
+++ task3/minirt/CMakeFiles/minirt.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minirt.dir/camera.cpp.o"
+  "CMakeFiles/minirt.dir/color.cpp.o"
+  "CMakeFiles/minirt.dir/image.cpp.o"
+  "CMakeFiles/minirt.dir/jpge.cpp.o"
+  "CMakeFiles/minirt.dir/material.cpp.o"
+  "CMakeFiles/minirt.dir/point_3d.cpp.o"
+  "CMakeFiles/minirt.dir/point_light.cpp.o"
+  "CMakeFiles/minirt.dir/ray.cpp.o"
+  "CMakeFiles/minirt.dir/sampler.cpp.o"
+  "CMakeFiles/minirt.dir/scene.cpp.o"
+  "CMakeFiles/minirt.dir/scene_loader.cpp.o"
+  "CMakeFiles/minirt.dir/sphere.cpp.o"
+  "CMakeFiles/minirt.dir/vector_3d.cpp.o"
+  "CMakeFiles/minirt.dir/view_plane.cpp.o"
+  "libminirt.a"
+  "libminirt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minirt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
