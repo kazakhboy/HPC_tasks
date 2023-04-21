8b714b9d6da1352f191a98b25e9ac4805de4134f
Muhamedali <79417700+kazakhboy@users.noreply.github.com>
2023-04-21 16:43:20 +0500

    Add files via upload

---
+++ task4/build/minirt/CMakeFiles/minirt.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/camera.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/camera.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/color.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/color.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/image.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/image.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/jpge.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/jpge.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/material.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/material.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/point_3d.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/point_3d.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/point_light.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/point_light.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/ray.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/ray.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/sampler.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/sampler.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/scene.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/scene.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/scene_loader.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/scene_loader.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/sphere.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/sphere.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/vector_3d.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/vector_3d.cpp.o"
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/view_plane.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/view_plane.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ task4/build/test/CMakeFiles/minirt-test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/test/minirt_test.cpp" "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/test/CMakeFiles/minirt-test.dir/minirt_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/mini-rt/src/minirt/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hpc2022/bda2001/AlexandrGavrilko/task2/build/minirt/CMakeFiles/minirt.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
